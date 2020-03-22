c4f00ae35011ec1303a68954dd5d6133116b257e
xiesh01 <xiesh01@mingyuanyun.com>
2020-03-22 21:15:14 +0800

    fitst commit

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-22 19:18:18
+ * @LastEditTime: 2020-03-22 20:44:38
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /my-react-ts-test/src/react-app-env.d.ts
+ */
 /// <reference types="react-scripts" />
+declare module "*.less" {
+  const less: any;
+  export default less;
+}
